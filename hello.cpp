e9b6d731dd1b6b2240bb4e7f325a4c73e396f985
nvarner <nathanmvarner@protonmail.com>
2021-09-16 19:44:51 -0400

    Add echo features to hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[]) {
 	std::cout << "Hello, world! Hamilton is amazing\n";
 	std::cout << "Learn more: https://www.youtube.com/watch?v=dQw4w9WgXcQ\n";
+
+        for (size_t i = 1; i < argc; i++) {
+            std::cout << argv[i];
+            if (i < argc - 1) {
+                std::cout << " ";
+            }
+        }
+        std::cout << "\n";
 }
