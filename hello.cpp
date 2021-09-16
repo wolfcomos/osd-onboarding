23e54167890eb8ecc2bde855a54064697d65bc82
nvarner <nathanmvarner@protonmail.com>
2021-09-16 19:48:39 -0400

    Refactor echo feature into separate function

--- hello.cpp
+++ hello.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
+void print_args(char* args[], int num_args) {
+    for (size_t i = 1; i < num_args; i++) {
+        std::cout << args[i];
+        if (i < num_args - 1) {
+            std::cout << " ";
+        }
+    }
+    std::cout << "\n";
+}
+
 int main(int argc, char *argv[]) {
 	std::cout << "Hello, world! Hamilton is amazing\n";
 	std::cout << "Learn more: https://www.youtube.com/watch?v=dQw4w9WgXcQ\n";
 
-        for (size_t i = 1; i < argc; i++) {
-            std::cout << argv[i];
-            if (i < argc - 1) {
-                std::cout << " ";
-            }
-        }
-        std::cout << "\n";
+        print_args(argv, argc);
 }
+
