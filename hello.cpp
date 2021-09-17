5c60ad807d7c06f9686227fa53b9a4a2307f2798
nvarner <nathanmvarner@protonmail.com>
2021-09-16 20:29:20 -0400

    whoops, pushed to wrong repo

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
 
-void print_args(char* args[], int num_args) {
-    for (size_t i = 1; i < num_args; i++) {
-        std::cout << args[i];
-        if (i < num_args - 1) {
-            std::cout << " ";
-        }
-    }
-    std::cout << "\n";
-}
-
-int main(int argc, char *argv[]) {
+int main() {
 	std::cout << "Hello, world! Hamilton is amazing\n";
 	std::cout << "Learn more: https://www.youtube.com/watch?v=dQw4w9WgXcQ\n";
-
-        print_args(argv, argc);
 }
-
